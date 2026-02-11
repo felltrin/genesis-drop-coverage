0432056e1c94cbbcbb8db8dcd33116f419e3b49d
felltrin <luciholzinger@gmail.com>
2026-02-11 07:38:30 -0800

    split the first string using a character as the delimeter

--- tutorials/hello-world.cs
+++ tutorials/hello-world.cs
@@ -10,3 +10,9 @@ Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
 
 Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
 Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+string[] firstFriendSplit = firstFriend.Split('a');
+for (int i = 0; i < firstFriendSplit.Length; i++)
+{
+    Console.WriteLine(firstFriendSplit[i]);
+}
